f7be7b092371575dee66cb92d04fa85bcdefb848
Victor Ozoterio <victorozoterio@gmail.com>
2025-10-20 16:52:42 -0300

    feat: adiciona sistema de rotas com layout padrão e página checkout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { ThemeProvider } from "styled-components";
-
 import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
-import { Header } from "./components/Header";
-import { Home } from "./pages/Home";
 import { CartProvider } from "./contexts/CartContext";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CartProvider>
-        <Header />
-        <Home />
+        <Router />
         <GlobalStyle />
       </CartProvider>
     </ThemeProvider>
